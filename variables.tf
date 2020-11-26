8ce3380d297e5f19cb62f9d225ee4d0d2868fb07
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-11-26 12:57:53 -0500

    Updated descriptions for other variables.

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "libvirt_disk_path" {
-  description = "path for libvirt pool"
+  description = "Path to create libvirt pool at."
   default = "/tmp/terraform-libvirt-pool"
 }
 
 variable "fedora_cloud_img_url" {
-  description = "fedora cloud image"
+  description = "URL for Fedora image."
   default = "https://download.fedoraproject.org/pub/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.qcow2"
 }
 
 variable "vm_hostname" {
-  description = "vm hostname"
+  description = "Hostname for the VM."
   default = "nginx"
 }
 
 variable "ssh_username" {
-  description = "the ssh user to use"
+  description = "SSH user to use"
   default = "fedora"
 }
 
