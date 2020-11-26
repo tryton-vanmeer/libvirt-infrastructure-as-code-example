96fe951e8c211846426742628a8327cfbe17d12d
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-11-26 12:57:07 -0500

    Added variable for public key alongside private SSH key.

--- variables.tf
+++ variables.tf
@@ -19,6 +19,11 @@ variable "ssh_username" {
 }
 
 variable "ssh_private_key" {
-  description = "the private key to use"
-  default = "./config/fedora_rsa"
+  description = "Location of SSH private key"
+  default = "~/.ssh/id_rsa"
+}
+
+variable "ssh_public_key" {
+  description = "Location of SSH public key."
+  default = "~/.ssh/id_rsa.pub"
 }
