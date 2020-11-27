8874f85afefbfcc6c68c91360b396f54994eb08b
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-11-27 11:06:13 -0500

    Added output example for dynamically generating Ansible inventories.

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,21 @@ output "url" {
 output "ssh" {
   value = "${var.ssh_username}@${var.hostname}.terraform.vm"
 }
+
+# Output an Ansible inventory, given the list of libvirt domains.
+# This can be used to run post-provision playbooks.
+resource "local_file" "AnsibleInventory" {
+  content = templatefile("${path.module}/templates/inventory.tmpl",
+  {
+    domains = [ libvirt_domain.nginx ]
+  }
+  )
+  filename = "inventory"
+}
+
+# The provision playbook could be modified to be more basic.
+# The nginx role could be applied after as post-provision.
+
+# With the dnsmasq trick for local libvirt virtual networks, inventories
+# could be written that don't need this generated one, since servers could be
+# targeted at `hostname.terraform.vm`.
